155ec17d7777e628251959b84470f0b5d15c83e3
Natalie B. <22222885+espeon@users.noreply.github.com>
2025-12-29 13:49:12 -0600

    maybe this will work

--- frontend/src/routes/legal.content-guidelines.tsx
+++ frontend/src/routes/legal.content-guidelines.tsx
@@ -13,7 +13,7 @@ function RouteComponent() {
         <h1 className="text-4xl font-semibold">Privacy Policy</h1>
         <p>Last updated Dec 29, 2025</p>
       </div>
-      <article className="prose-invert lg:prose-lg dark:prose-invert prose-headings:font-bold prose-a:text-primary hover:prose-a:underline text-wrap">
+      <article className="prose prose-invert lg:prose-lg dark:prose-invert prose-headings:font-bold prose-a:text-primary hover:prose-a:underline text-wrap">
         <Markdown>{content}</Markdown>
       </article>
     </div>
